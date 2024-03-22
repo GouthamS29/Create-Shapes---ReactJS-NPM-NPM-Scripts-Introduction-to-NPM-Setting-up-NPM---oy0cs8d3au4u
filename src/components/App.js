6ced8974bc3c7a8f3cd67f74f3663b575fd780ca
GouthamS29 <sambarajugoutham1994@gmail.com>
2024-03-22 11:14:25 +0530

    Create Shapes App Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
+
 const App = () => {
+  const [shapes, setShapes] = useState([]);
+  const [selected, setSelected] = useState("square");
+
+  const handleAddShape = () => {
+    const oldShapes = [...shapes];
+    oldShapes.push(selected);
+    setShapes(oldShapes);
+  };
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange= {(e) => setSelected(e.target.value)} value={selected} >
+          <option value="square">Sqaure</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={handleAddShape}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {shapes.map((shape, index) => {
+            return (
+              <div className={shape} key={index}>
+                {index}
+              </div>
+            );
+          })}
       </div>
     </div>
   )
